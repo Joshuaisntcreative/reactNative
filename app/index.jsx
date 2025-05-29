cdb0bfa19b9501d9771dd8afcd8bba11c55240d8
Joshuaisntcreative <joshuajainarine@gmail.com>
2025-05-28 23:13:42 -0400

    added working/ styled icons in the dashboard folder with the tabs feature

--- app/index.jsx
+++ app/index.jsx
@@ -25,6 +25,11 @@ const Home = () => {
         <ThemedText>Register Page</ThemedText>
         </Link>
 
+
+        <Link href = "/profile" style = {styles.link}>
+        <ThemedText>Profile Page</ThemedText>
+        </Link>
+
     </ThemedView>
   )
 }
